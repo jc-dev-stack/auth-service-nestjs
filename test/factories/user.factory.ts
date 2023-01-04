9789aa9bbca036b4e3748370810fa74536c85c82
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-04 12:42:35 -0300

    created user factory

---
+++ test/factories/user.factory.ts
@@ -0,0 +1,16 @@
+import { PropsUserEntity, User } from './../../src/entities/user.entity';
+type Override = Partial<PropsUserEntity>
+
+export class UserFactory {
+    static make(override: Override) {
+        return (
+            new User({
+                id: 1,
+                name: 'user',
+                login: 'user',
+                password: '$2y$10$/.KTCCYjquhWidBTq/GfqOW7jcJnOrfp/1Fl81CejkR0wSBbm6Plu', // password
+                ...override
+            })
+        )
+    }
+}
