87c2ee2bc09559d8bd920df3df0efed078805afb
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-05 23:49:13 -0300

    database in memory

---
+++ test/database/repositories/user.repository.memory.ts
@@ -0,0 +1,20 @@
+import { UserRepositoryContract } from 'src/app/repositories/user.repositoy.contract';
+import { User } from './../../../src/app/entities/user.entity';
+
+export class UserRepositoryMemory implements UserRepositoryContract {
+
+    public users: User[] = [];
+
+    async list(): Promise<User[]> {
+        return this.users;
+    }
+    async findByLogin(login: string): Promise<User> {
+        const index = this.users.findIndex(user => user.login === login);
+        return this.users[index];
+    }
+    async create(user: User): Promise<User> {
+        this.users.push(user);
+        return user;
+    }
+
+}
