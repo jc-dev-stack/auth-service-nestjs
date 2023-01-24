3fc2850844b6fd39380030139c10139b7f746664
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-24 08:37:53 -0300

    auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) { }
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const { user } = await this.userService.findByLogin(username);
+        if (user && user.password == pass) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+}
