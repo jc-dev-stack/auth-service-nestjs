2a37a0a79a81232351cf57e4188a86b80f3d368a
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-24 09:09:25 -0300

    ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { UserModule } from './app/user/user.module';
-import { AuthModule } from './auth/auth.module';
 import { AuthModule } from './app/auth/auth.module';
 
 @Module({
