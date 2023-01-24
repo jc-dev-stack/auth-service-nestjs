6fff8d1b5c600924f96402441e1a9a06f349420c
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-24 09:23:26 -0300

    jwt service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../entities/user.entity';
 import { UserService } from '../user/user.service';
 
+export interface Payload {
+    username: string
+    sub: number
+}
+
+export interface ResponseLogin {
+    access_token: string
+}
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) { }
 
     async validateUser(username: string, pass: string): Promise<any> {
         const { user } = await this.userService.findByLogin(username);
@@ -12,6 +25,13 @@ export class AuthService {
             return result;
         }
         return null;
+    }
+
+    async login(user: User): Promise<ResponseLogin> {
+        const payload: Payload = { username: user.login, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
     }
 
 }
