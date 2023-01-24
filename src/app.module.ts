6f6997554f55634ad0d65e23fee9d7cf0ec2c4b2
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-24 08:34:33 -0300

    create module and service auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { UserModule } from './app/user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './app/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule]
+  imports: [DatabaseModule, UserModule, AuthModule]
 })
 export class AppModule { }
