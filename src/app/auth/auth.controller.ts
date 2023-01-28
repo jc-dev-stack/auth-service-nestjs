12e8ef9e50a5df04607587be4c618ff57d9486ac
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-27 23:07:11 -0300

    auth strategy e auth guard

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
@@ -15,5 +16,11 @@ export class AuthController {
     @Post('/login')
     async login(@Request() req: RequestUser) {
         return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/profile')
+    async getProfile(@Request() req: RequestUser) {
+        return req.user
     }
 }
